fb5f6c098ac05dcbb5c346aac2a2a6817a5a0c81
Lucas Romulo Souza <lucas.romulo.sr@gmail.com>
2021-03-31 13:15:48 -0300

    esqueleto do model

---
+++ model/departamento.py
@@ -0,0 +1,30 @@
+class Departamento:
+	def __init__(self, nome):
+		self.nome = nome
+		self.cursos = []
+		self.funcionarios = []
+		self.documentos = []
+
+	def getNome():
+		return self.nome
+
+	def setNome(nome):
+		self.nome = nome
+
+	def getCursos():
+		return self.cursos
+
+	def setCursos(cursos):
+		self.cursos = cursos
+
+	def getFuncionarios():
+		return self.funcionarios
+
+	def setFuncionarios(funcionarios):
+		self.funcionarios = funcionarios
+
+	def getDocumentos():
+		return self.documentos
+
+	def setDocumentos(documentos):
+		self.documentos = documentos
---
+++ model/documento.py
@@ -0,0 +1,16 @@
+class Documento:
+	def __init__(self, nome, caminho):
+		self.nome = nome
+		self.caminho = caminho
+
+	def getNome():
+		return self.nome
+
+	def setNome(nome):
+		self.nome = nome
+
+	def getCaminho():
+		return self.caminho
+
+	def setCaminho(caminho):
+		set.caminho = caminho
